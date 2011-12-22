bac909ea1f8f0b954dea72b4bdedffc005aa2e27
Bernard Lambeau <blambeau@gmail.com>
2011-12-22 19:28:54 +0100

    Review migrate script

--- scripts/migrate.rb
+++ scripts/migrate.rb
@@ -1,13 +1,39 @@
 #!/usr/bin/env ruby
+
+def change(x)
+  puts x
+  x
+end
+
+def link(link)
+  case link
+    when "/", /^http/, /^mailto:/
+      link
+    else
+      "/#{link}/"
+  end
+end
+
 dir = File.dirname(File.dirname(__FILE__))
 Dir["#{dir}/**/index.yml"].each do |file|
   begin
     before = File.read(file)
-    if before =~ rx
-      puts "Rewriting #{file}"
-      File.open(file, 'w') do |io|
-        io << before.gsub(rx, to)
-      end
+    before.gsub!(/@\{(.*?)\}\[(.*?)\]/) do |match|
+      "@{#{link($2)}}{#{$1}}"
+    end
+    before.gsub!(/\{(.*?)\}\[(.*?)\]/) do |match|
+      "@{#{link($2)}}{#{$1}}"
+    end
+    before.gsub!(/===/) do |match|
+      "###"
+    end
+    before.gsub!(/==/) do |match|
+      "##"
+    end
+
+    puts "Rewriting #{file}"
+    File.open(file, 'w') do |io|
+      io << before
     end
   rescue => ex
     puts "Skipping #{file}: #{ex.message}"
