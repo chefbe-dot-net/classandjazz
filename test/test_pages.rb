df026ab3b375fa9526d7475ed32b658ee7c60cd6
Bernard Lambeau <blambeau@gmail.com>
2011-12-22 22:11:30 +0100

    Fix a text

--- test/test_pages.rb
+++ test/test_pages.rb
@@ -2,12 +2,12 @@ require 'webapp_test'
 class LangTest < WebAppTest
 
   def test_default_language
-    visit('/home')
+    visit('/')
     assert_equal "nl", first("html")["lang"]
   end
 
   def test_english
-    visit('/home?lang=en')
+    visit('/?lang=en')
     assert_equal "en", first("html")["lang"]
   end
 
